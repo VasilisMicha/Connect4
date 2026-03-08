299597a3d6af10fee3b1998ffcef262aad6c5e37
VasilisMicha <Vasilismich789@gmail.com>
2026-03-08 19:25:44 +0200

    agent vs random bot

--- connect_four.py
+++ connect_four.py
@@ -8,17 +8,17 @@ rows: Final = 6
 columns: Final = 7
 
 class Turn(Enum):
-    PLAYER1 = 1
-    PLAYER2 = -1
+    AGENT = 1
+    OPPONENT = 2
 
 class ConnectFour(gym.Env):
     def __init__(self):
-        self.board = np.zeros([rows, columns], dtype=np.int_)
-        self.turn = random.choice(list(Turn))
-        self.reward = 0
-        self.terminated = False
         self.action_space = gym.spaces.Discrete(columns)
         self.observation_space = gym.spaces.Box(low=0, high=1, shape=(3, rows, columns))
+
+
+    def get_turn(self):
+        return self.turn
 
 
     def get_terminated(self):
@@ -26,48 +26,50 @@ class ConnectFour(gym.Env):
 
 
     def step(self, action):
-        column = action
-        row = self.find_slot(column)
-        self.insert(row, column)
-        print(self.board)
-        print(self.turn)
+        # Agent's turn
+        self.turn = Turn.AGENT
+
+        row1 = self.find_slot(action)
+        self.insert(row1, action)
+        self.print_info(action)
+        self.check_game_completion(row1, action)
+        if self.terminated:
+            return self.board_to_tensor(), self.reward, self.terminated, False, {}
+
+        # Opponent's turn
+        self.turn = Turn.OPPONENT
+
+        column = self.opponent_action()
+        row2 = self.find_slot(column)
+        self.insert(row2, column)
+        self.print_info(column)
+        self.check_game_completion(row2, column)
         
-        # terminate if the board is full or a connect four was achieved
-        if (self.connect_four(row, column)):
-            print(f"{self.turn.name} won!")
-            self.terminated = True
-            if self.turn == Turn.PLAYER1:
-                self.reward = 1
-            else:
-                self.reward = -1
-        elif not (np.any(self.board[0] == 0)):
-            print("Board is full!")
-            self.terminated = True
-
-
-        if self.turn == Turn.PLAYER1:
-            self.turn = Turn.PLAYER2
-        elif self.turn == Turn.PLAYER2:
-            self.turn = Turn.PLAYER1
-
-        return self.board_to_tensor()
+        self.turn = Turn.AGENT
+        return self.board_to_tensor(), self.reward, self.terminated, False, {}
 
 
     def reset(self, *, seed: int | None = None, options: dict | None = None):
         super().reset(seed=seed)
-        self.board = np.zeros([rows, columns], dtype=np.int_)
+        self.board = np.zeros([rows, columns])
         self.turn = random.choice(list(Turn))
+        if (self.turn == Turn.OPPONENT):
+            column = self.opponent_action()
+            row = self.find_slot(column)
+            self.insert(row, column)
+            self.print_info(column)
         self.reward = 0
         self.terminated = False
 
+        self.turn = Turn.AGENT
         return self.board_to_tensor(), {}
 
 
     def insert(self, row, column):
-        if (self.turn == Turn.PLAYER1):
-            self.board[row][column] = Turn.PLAYER1.value
-        elif (self.turn == Turn.PLAYER2):
-            self.board[row][column] = Turn.PLAYER2.value
+        if (self.turn == Turn.AGENT):
+            self.board[row][column] = Turn.AGENT.value
+        elif (self.turn == Turn.OPPONENT):
+            self.board[row][column] = Turn.OPPONENT.value
         else:
             raise Exception("Wrong turn value")
 
@@ -88,6 +90,20 @@ class ConnectFour(gym.Env):
         return j
 
 
+    def check_game_completion(self, row, column):
+        if (self.connect_four(row, column)):
+            print(f"{self.turn.name} won!")
+            self.terminated = True
+
+            if self.turn == Turn.AGENT:
+                self.reward = 1
+            else:
+                self.reward = -1
+        elif not (np.any(self.board[0] == 0)):
+            print("It's a Draw, the Board is full!")
+            self.terminated = True
+
+
     def connect_four(self, row, column):
         directions = []
         directions.append(self.board[:, column]) # vertical
@@ -96,39 +112,54 @@ class ConnectFour(gym.Env):
         directions.append(np.diag(np.fliplr(self.board), k= (columns - column -1) - row)) # negative diagonal
 
         for dir in directions:
+            dir = map(int, dir)
             dir_to_str = "".join(map(str, dir))
             # if players disc appears 4 times in a row in a direction
+            print(f"{str(self.turn.value)*4} - {dir_to_str}")
             if (str(self.turn.value)*4 in dir_to_str):
                 return True
         
         return False
 
 
+    def opponent_action(self):
+        return self.random_action()
+
+
+    def random_action(self):
+        masks = self.get_valid_actions();
+        indexes = np.where(masks == 1)
+
+        action = np.random.choice(indexes[0])
+        return action
+
+
+
     def board_to_tensor(self):
-        player = (self.board == self.turn.value).astype(int)
-        opponent = (self.board == -self.turn.value).astype(int)
+        agent = (self.board == Turn.AGENT.value).astype(np.float32)
+        opponent = (self.board == Turn.OPPONENT.value).astype(np.float32)
 
-        value = 1.0 if self.turn == Turn.PLAYER1 else 0.0
-        turn_indicator = np.full((rows, columns), value, dtype=np.float32)
-
-        return np.stack([player, opponent, turn_indicator], axis=0)
+        return np.stack([agent, opponent], axis=0)
 
 
     def get_valid_actions(self):
         return (self.board[0] == 0).astype(int)
 
 
-    def print_board(self):
+    def print_info(self, column):
         print(self.board)
+        print(self.turn)
+        print(column)
 
 
-b = ConnectFour()
+c4 = ConnectFour()
 ch = 'y'
 while ch != 'n':
-    while not b.get_terminated():
-        column = int(input("Choose column: "))
-        b.step(column)
+    c4.reset()
 
+    while not c4.get_terminated():
+        column = int(input("Choose column: "))
+        c4.step(column)
 
     ch = input("Want to continue? (y/n): ")
 
