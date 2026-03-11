155b40ae026d0fa41ca4e7d16e020d69b5d1e8e0
Vasileios Michailidis <mkvasile@csd.auth.gr>
2026-03-11 12:36:57 +0200

    implemented agent's optimize()

--- agent.py
+++ agent.py
@@ -1,9 +1,11 @@
 from collections import namedtuple, deque
+import torch.optim as optim
 from model import DQN
 import numpy as np
 import torch
 import math
 import random
+import torch.nn as nn
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
@@ -40,7 +42,8 @@ class DQNAgent:
         self.tau = tau
         self.lr = lr
         self.policy_net = DQN(actions=self.action_size).to(device)
-        self.targe_net = DQN(actions=self.action_size).to(device)
+        self.target_net = DQN(actions=self.action_size).to(device)
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.lr, amsgrad=True)
         self.steps_done = 0
         self.device = device
         self.env = env
@@ -65,7 +68,7 @@ class DQNAgent:
 
     def store_transition(self, state, action, stats):
         next_state = stats[0].unsqueeze(0)
-        reward = torch.tensor(stats[1])
+        reward = torch.tensor([[stats[1]]])
         terminated = stats[2]
         if terminated:
             self.memory.push(state, action, None, reward)
@@ -78,13 +81,35 @@ class DQNAgent:
             return
         transitions = self.memory.sample(self.batch_size)
         batch = Transition(*zip(*transitions))
-        # Sampling: It pulls a random batch (e.g., 128 transitions) from memory.
-        #
-        # Target Calculation: It uses the Target Network to calculate what the future reward should have been according to the Bellman Equation.
-        #
-        # Loss & Backprop: It calculates the difference (Loss) between the policy_net prediction and the target, then updates the policy_net weights.
+
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=self.device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        next_state_values = torch.zeros((self.batch_size, 1), device=self.device)
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1).values.unsqueeze(1)
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values)
+
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.lr, amsgrad=True)
+        self.optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
 
 
     def update_target_network(self):
-        pass
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+        target_net.load_state_dict(target_net_state_dict)
         # Soft Update: It uses your TAU ($\tau$) value to slowly merge the policy_net weights into the target_net.Stability: This ensures the target values don't jump around too much, which prevents the training from becoming chaotic.
